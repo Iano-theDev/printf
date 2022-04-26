aa52b976d98211c94e0c015c966ecfd0bf85c573
MomanyiJ <momanyij04@gmail.com>
2022-04-26 12:11:54 -0700

    corrections on conflict

--- bin.c
+++ bin.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"                                                                                                                                                    
 /**                                                                                                                                                                  
  * bin - converts to binary                                                                                                                                          
@@ -19,22 +18,3 @@ int bin(unsigned int n, int *ptr)
         write(1, &c, 1);                                                                                                                                             
         return (*ptr);                                                                                                                                               
 } 
-=======
-#include "main.h"
-/**
- * bin - converts to binary
- * @n: input
- * Return: No return value
- */
-void bin(unsigned int n)
-{
-	char c;
-
-	if (n / 2 > 0)
-	{
-		bin(n / 2);
-	}
-	c = (n % 2) + '0';
-	write(1, &c, 1);
-}
->>>>>>> d0d7ec91cd7b9d6b1e54853a3d85fcfdb8846604
